e333173a65e9329b60a0febc4a31e0999eb25b56
Hannaneh Rezaei <hannanehrezaei17@gmail.com>
2025-09-12 09:05:35 +0330

    fix(password_generator)

--- 01-password-generator/main.py
+++ 01-password-generator/main.py
@@ -27,13 +27,3 @@ class RandomPassword(PasswordGenerator):
 
     def generate(self):
         return ''.join(secrets.choice(self.characters) for _ in range(self.length))
-    
-# ساختن آبجکت از کلاس‌ها
-pin = PinPassword(length=6)               # پسورد فقط عددی، ۶ رقمی
-rand1 = RandomPassword(length=12)         # پسورد شامل حروف+اعداد+سیمبل
-rand2 = RandomPassword(length=10, numbers=False, symbols=False)  # فقط حروف
-
-# تست خروجی‌ها
-print("PIN Password:", pin.generate())
-print("Random Password (default):", rand1.generate())
-print("Random Password (letters only):", rand2.generate())
